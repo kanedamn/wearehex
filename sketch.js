0f49bca08f08c9a24d6240e49891fdb2f2dbbf33
Martina <martinamelillo97@gmail.com>
2019-12-01 11:31:36 +0100

    404

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ var pass;
 var advice;
 var myImg;
 var error;
+
 var show = false;
 var wrong = false;
 
@@ -12,42 +13,42 @@ function preload(){
   error = loadImage("./assets/error.png");
 }
 
-var timeLeft = function(){
-    var currentDate = new Date();
-    currentDate = currentDate.getTime();
-    var endDate = new Date("2019-11-29T00:00:00");
-    if (parseInt((endDate-currentDate)/1000)<0){return 0};
-    return parseInt((endDate-currentDate)/1000);
-}
-
-var timerText = function(){
-    var h = String(parseInt(timer / 3600));
-    var m = String(parseInt(timer / 60)%60);
-    var s = String(timer - 3600*h - 60*m);
-
-    if (h.length == 1){h="0"+h;};
-    if (m.length == 1){m="0"+m;};
-    if (s.length == 1){s="0"+s;};
-
-
-    return h+":"+m+":"+s;
-
-}
-
-function countDown(){
-    textAlign(CENTER, CENTER);
-    textSize(100);
-    textFont(font);
-    fill(255, 0, 0);
-    text(timerText(), width/2, height/2 - 150);
-}
-
-
-function fireshow(){
-    background(0);
-}
-
-var timer = timeLeft();
+// var timeLeft = function(){
+//     var currentDate = new Date();
+//     currentDate = currentDate.getTime();
+//     var endDate = new Date("2019-11-29T00:00:00");
+//     if (parseInt((endDate-currentDate)/1000)<0){return 0};
+//     return parseInt((endDate-currentDate)/1000);
+// }
+//
+// var timerText = function(){
+//     var h = String(parseInt(timer / 3600));
+//     var m = String(parseInt(timer / 60)%60);
+//     var s = String(timer - 3600*h - 60*m);
+//
+//     if (h.length == 1){h="0"+h;};
+//     if (m.length == 1){m="0"+m;};
+//     if (s.length == 1){s="0"+s;};
+//
+//
+//     return h+":"+m+":"+s;
+//
+// }
+//
+// function countDown(){
+//     textAlign(CENTER, CENTER);
+//     textSize(100);
+//     textFont(font);
+//     fill(255, 0, 0);
+//     text(timerText(), width/2, height/2 - 150);
+// }
+//
+//
+// function fireshow(){
+//     background(0);
+// }
+//
+// var timer = timeLeft();
 
 function setup(){
     createCanvas (innerWidth, innerHeight);
@@ -91,15 +92,21 @@ function goToPage(){
 function draw() {
     background (0);
 
-    if (frameCount % 60 == 0 && timer > 0) {
-        timer --;
-    }
+    // if (frameCount % 60 == 0 && timer > 0) {
+    //     timer --;
+    // }
+    //
+    // if (timer == 0) {
+    //     fireshow();
+    // } else {
+    //     countDown();
+    // }
 
-    if (timer == 0) {
-        fireshow();
-    } else {
-        countDown();
-    }
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    textFont(font);
+    fill(255, 0, 0);
+    text("404", width/2, height/2 - 150);
 
     textFont(font);
     textSize(50);
